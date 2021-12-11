831e8c23a36439691ea26c78656fda3848ed0b9b
Cláudio Vitor Dantas <65822856+CDInacio@users.noreply.github.com>
2021-12-10 21:00:34 -0300

    update addToCart function logic

--- backend/src/controllers/cartController.js
+++ backend/src/controllers/cartController.js
@@ -1,10 +1,18 @@
-const cart = require("../models/Cart");
+const jwt = require("jsonwebtoken");
+
+const Cart = require("../models/Cart");
 
 module.exports.addToCart = async (req, res) => {
-  const { items, totalQuantity, costumerId } = req.body;
-  await cart.create({
+  const { items, totalQuantity, costumerEmail } = req.body;
+  const cartData = await Cart.findOne({ costumerEmail: costumerEmail });
+  const cart = {
     items,
     totalQuantity,
-    costumerId
-  });
+    costumerEmail,
+  };
+  if (cartData === null) {
+    await Cart.create(cart);
+  } else {
+    await Cart.updateOne({ costumerEmail: costumerEmail }, cart);
+  }
 };
